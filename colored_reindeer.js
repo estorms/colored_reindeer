b8a8c42f8c235d742b007028977c6aae25c015f0
Elizabeth Sanger <libsang@gmail.com>
2016-07-09 09:04:12 -0500

    First commit

---
+++ colored_reindeer.js
@@ -0,0 +1,21 @@
+var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+
+var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+
+coloredReindeer =[];
+
+for (var i = 0; i < reindeer.length; i++) {
+coloredReindeer.push(colors[i] + ' ' + reindeer[i]);
+}
+console.log(coloredReindeer);
+
+/*  for (var i = 0; i < colors.length; i++) {
+    for (var j = 0; j < reindeer.length; j++) {
+    if (colors[i] === reindeer[j]) {
+        coloredReindeer.push(colors[i] + reindeer[j]);
+    }
+    }
+    console.log(coloredReindeer);
+    }
+
+*/
